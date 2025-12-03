3004ce25705e79310f6dece83375e11ef1b26cb1
stivenzxy <jesuspereztorres420@gmail.com>
2025-12-03 02:05:55 -0500

    changes in manifests cleark

--- microservices/new/app.py
+++ microservices/new/app.py
@@ -6,13 +6,12 @@ import uuid
 import os
 app = FastAPI()
 
-# Conexión a Redis usando la variable de entorno
 redis_client = redis.Redis(
     host=os.getenv("REDIS_HOST", "redis"),
     port=6379,
     db=0
 )
-# Postgres connection
+
 conn = psycopg2.connect(
     host=os.getenv("POSTGRES_HOST", "postgres"),
     database=os.getenv("POSTGRES_DB", "primesdb"),
@@ -27,17 +26,14 @@ class RequestData(BaseModel):
 
 @app.post("/new")
 def new_request(data: RequestData):
-    # Crear ID único de solicitud
     request_id = str(uuid.uuid4())
 
-    # Guardar solicitud en DB
     with conn.cursor() as cur:
         cur.execute(
             "INSERT INTO requests (id, total_requested) VALUES (%s, %s)",
             (request_id, data.cantidad)
         )
 
-    # Encolar solicitudes individuales
     for _ in range(data.cantidad):
         redis_client.lpush("prime_queue", f"{request_id}:{data.digitos}")
 
